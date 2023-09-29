4390bc29cd805ed1beab4eafd2db70b3d3c17e0e
goodmorningmint <142309546+goodmorningmint@users.noreply.github.com>
2023-09-30 00:32:26 +0700

    Update code.js

--- public/code.js
+++ public/code.js
@@ -1,5 +1,6 @@
 (function(){
     let receiverID;
+    let roomID;
     const socket = io("http://localhost:3000");
 
     function generateID(){
@@ -26,47 +27,58 @@
     });
 
     document.querySelector("#file-input").addEventListener("change", function(e){
-        let file = e.target.files[0];
-        if (!file) {
+        let files = e.target.files;
+        if (!files || files.length === 0) {
             return;
         }
-        let reader = new FileReader();
-        reader.onload = function(e){
-            let buffer = new Uint8Array(reader.result);
-            let el = document.createElement("div");
-            el.classList.add("item");
-            el.innerHTML = `
-                <div class="progress">0%</div>
-                <div class="filename">${file.name}</div>
-            `;
-            document.querySelector(".files-list").appendChild(el);
-            shareFile({
-                filename: file.name,
-                total_buffer_size: buffer.length,
-                buffer_size: 1024
-            }, buffer, el.querySelector(".progress"));
-        };
-        reader.readAsArrayBuffer(file);
+        
+        for (let i = 0; i < files.length; i++) {
+            let file = files[i];
+            let reader = new FileReader();
+            reader.onload = function(e){
+                let buffer = new Uint8Array(reader.result);
+                let el = document.createElement("div");
+                el.classList.add("item");
+                el.innerHTML = `
+                    <div class="progress">0%</div>
+                    <div class="filename">${file.name}</div>
+                `;
+                document.querySelector(".files-list").appendChild(el);
+                shareFile({
+                    filename: file.name,
+                    total_buffer_size: buffer.length,
+                    buffer_size: 1024
+                }, buffer, el.querySelector(".progress"));
+            };
+            reader.readAsArrayBuffer(file);
+        }
     });
 
-    function shareFile(metadata,buffer,progress_node){
-		socket.emit("file-meta", {
-			uid:receiverID,
-			metadata:metadata
-		});
-		
-		socket.on("fs-share",function(){
-			let chunk = buffer.slice(0,metadata.buffer_size);
-			buffer = buffer.slice(metadata.buffer_size,buffer.length);
-			progress_node.innerText = Math.trunc(((metadata.total_buffer_size - buffer.length) / metadata.total_buffer_size * 100))+ "%";
-			if(chunk.length != 0){
-				socket.emit("file-raw", {
-					uid:receiverID,
-					buffer:chunk
-				});
-			} else {
-				console.log("Sent file successfully");
-			}
-		});
-	}
+    function shareFile(metadata, buffer, progress_node) {
+        socket.emit("file-meta", {
+            uid: receiverID,
+            metadata: metadata
+        });
+
+        let chunkSize = metadata.buffer_size;
+        let offset = 0;
+
+        function sendChunk() {
+            let chunk = buffer.slice(offset, offset + chunkSize);
+            offset += chunkSize;
+            progress_node.innerText = Math.min(100, Math.trunc((offset / metadata.total_buffer_size) * 100)) + "%";
+
+            if (chunk.length !== 0) {
+                socket.emit("file-raw", {
+                    uid: receiverID,
+                    buffer: chunk
+                });
+                requestAnimationFrame(sendChunk);
+            } else {
+                console.log("Sent file successfully");
+            }
+        }
+
+        sendChunk();
+    }
 })();

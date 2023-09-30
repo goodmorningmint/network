533510fc943d35cd4d407ff2006f8c309cbc0950
goodmorningmint <pornkanok.t@kkumail.com>
2023-09-30 23:58:29 +0700

    LAST COMMIT

--- public/receiver.js
+++ public/receiver.js
@@ -1,59 +1,71 @@
 (function(){
     const socket = io("http://localhost:3000");
     let sender_uid;
-    let fileShare = {};
 
-	function generateID(){
+    function generateID(){
         return `${Math.trunc(Math.random()*999)}-${Math.trunc(Math.random()*999)}-${Math.trunc(Math.random()*999)}`;
     }
+
     document.querySelector("#receiver-start-con-btn").addEventListener("click",function(){
-		sender_uid = document.querySelector("#join-id").value;
-		if(sender_uid.length == 0){
-			return;
-		}
-		let joinID = generateID();
-		socket.emit("receiver-join", {
-			sender_uid:sender_uid,
-			uid:joinID
-		});
-		document.querySelector(".join-screen").classList.remove("active");
-		document.querySelector(".fs-screen").classList.add("active");
-	});
+        sender_uid = document.querySelector("#join-id").value;
+        if(sender_uid.length == 0){
+            return;
+        }
+        let joinID = generateID();
+        socket.emit("receiver-join", {
+            sender_uid:sender_uid,
+            uid:joinID
+        });
+        document.querySelector(".join-screen").classList.remove("active");
+        document.querySelector(".fs-screen").classList.add("active");
+    });
 
-	socket.on("fs-meta",function(metadata){
-		fileShare.metadata = metadata;
-		fileShare.transmitted = 0;
-		fileShare.buffer = [];
+    socket.on("fs-meta",function(metadata){
+        let el = document.createElement("div");
+        el.classList.add("item");
+        el.innerHTML = `
+            <div class="progress">0%</div>
+            <div class="filename">${metadata.filename}</div>
+            <button class="download-btn">Download</button>
+        `;
+        document.querySelector(".files-list").appendChild(el);
 
-		let el = document.createElement("div");
-		el.classList.add("item");
-		el.innerHTML = `
-				<div class="progress">0%</div>
-				<div class="filename">${metadata.filename}</div>
-		`;
-		document.querySelector(".files-list").appendChild(el);
+        let fileShare = {
+            metadata: metadata,
+            buffer: [],
+            transmitted: 0
+        };
 
-		fileShare.progrss_node = el.querySelector(".progress");
+        let progressNode = el.querySelector(".progress");
+        let downloadButton = el.querySelector(".download-btn");
 
-		socket.emit("fs-start",{
-			uid:sender_uid
-		});
-	});
+        downloadButton.addEventListener("click", function() {
+            if (fileShare.transmitted === fileShare.metadata.total_buffer_size) {
+                const fileBlob = new Blob(fileShare.buffer);
+                const downloadLink = document.createElement("a");
+                downloadLink.href = URL.createObjectURL(fileBlob);
+                downloadLink.download = fileShare.metadata.filename;
+    
+                document.body.appendChild(downloadLink);
+                downloadLink.click();
+    
+                document.body.removeChild(downloadLink);
+            }
+        });
 
-	socket.on("fs-share",function(buffer){
-		console.log("Buffer", buffer);
-		fileShare.buffer.push(buffer);
-		fileShare.transmitted += buffer.byteLength;
-		fileShare.progrss_node.innerText = Math.trunc(fileShare.transmitted / fileShare.metadata.total_buffer_size * 100)+ "%";
-		if(fileShare.transmitted == fileShare.metadata.total_buffer_size){
-			console.log("Download file: ", fileShare);
-			download(new Blob(fileShare.buffer), fileShare.metadata.filename);
-			fileShare = {};
-		} else {
-			socket.emit("fs-start",{
-				uid:sender_uid
-			});
-		}
-	});
+        socket.emit("fs-start",{
+            uid: sender_uid
+        });
 
+        socket.on("fs-share", function(buffer) {
+            fileShare.buffer.push(buffer);
+            fileShare.transmitted += buffer.byteLength;
+			const progressPercentage = Math.min(100, Math.trunc((fileShare.transmitted / fileShare.metadata.total_buffer_size) * 100));
+            progressNode.innerText = `${progressPercentage}%`;
+
+            if (fileShare.transmitted === fileShare.metadata.total_buffer_size) {
+                downloadButton.disabled = false;
+            }
+        });
+    });
 })();
--- server.js
+++ server.js
@@ -12,7 +12,12 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // หรือระบุโดเมนที่อนุญาต
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 
 
